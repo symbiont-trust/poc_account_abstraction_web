91c03751cb17132015b31030c2e4d79084bc86e0
Brivvins <markbrivvins@gmail.com>
2025-07-31 03:39:37 +0300

    1 - Added connect button

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <w3m-button />
+    </div>
+  )
+}
+
+export default App
